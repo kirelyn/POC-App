9ff2ab6c5b7c9e8747fd9d4381773ccb53c894d3
Kennedy Griffin <kennedyg@ksu.edu>
2020-07-16 16:58:31 -0500

    add query for priority

--- POCAPP/MySql/Query/returnable_query/same_class_and_club.sql
+++ POCAPP/MySql/Query/returnable_query/same_class_and_club.sql
@@ -1,15 +1,23 @@
-SELECT DISTINCT s.id
-FROM student AS s, class AS class, class_schedule AS csched, club AS club, club_schedule AS clubsched
+SELECT d.id, SUM(d.c) AS Count
+FROM (
+SELECT s.id, count(*) AS c 
+FROM student AS s 
+join class_schedule AS csched on s.id = csched.student_id
 JOIN (SELECT class_id
     FROM class_schedule
     WHERE student_id = 10) AS classes
 WHERE csched.class_id = classes.class_id
 AND s.id != 10
+GROUP BY s.id
 UNION ALL
-SELECT DISTINCT s.id
-FROM student AS s, class AS class, class_schedule AS csched, club AS club, club_schedule AS clubsched
+SELECT s.id, count(*) AS count2
+FROM student AS s
+JOIN club_schedule AS clubsched ON s.id = clubsched.student_id
 JOIN (SELECT club_id
     FROM club_schedule
     WHERE student_id = 10) AS clubs
 WHERE clubsched.club_id = clubs.club_id
-AND s.id != 10;
+AND s.id != 10
+GROUP BY s.id
+) AS d
+GROUP BY d.id;
--- POCAPP/MySql/Query/returnable_query/same_club.sql
+++ POCAPP/MySql/Query/returnable_query/same_club.sql
@@ -1,7 +1,12 @@
 SELECT DISTINCT s.id
-FROM student AS s, class AS class, class_schedule AS csched, club AS club, club_schedule AS clubsched
+FROM student AS s
+JOIN club_schedule AS clubsched ON s.id = clubsched.student_id
 JOIN (SELECT club_id
     FROM club_schedule
     WHERE student_id = 10) AS clubs
 WHERE clubsched.club_id = clubs.club_id
 AND s.id != 10;
+
+
+
+
