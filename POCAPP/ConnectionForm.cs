af5962c2978bb9d65342d0b68f184a6e2e181e4d
JohnnyZ67 <zielinskij@hotmail.com>
2020-07-17 12:28:14 -0500

    spelling change and database setup for me

--- POCAPP/ConnectionForm.cs
+++ POCAPP/ConnectionForm.cs
@@ -33,13 +33,12 @@ namespace POCAPP
         public void button1_Click(object sender, EventArgs e)
         {
             //int id = 10;
-            string connectionString = "server=localhost;database=pocapp;uid=Kennedy;pwd=Django100.;";
-            MySqlConnection con = new MySqlConnection(connectionString); 
-
+            string connectionString = "server=localhost;user id=johnny;pwd=W!ldfire24$;database=mydatabase";
+            MySqlConnection con = new MySqlConnection(connectionString);
             try
             {
                 con.Open();
-                MessageBox.Show("Connetion Successful!");
+                MessageBox.Show("Connection Successful!");
                 string sql = "SELECT d.id, SUM(d.c) AS Count FROM( SELECT s.id, count(*) AS c FROM student AS s join class_schedule AS csched on s.id = csched.student_id JOIN(SELECT class_id FROM class_schedule WHERE student_id = 10) AS classes WHERE csched.class_id = classes.class_id AND s.id != 10 GROUP BY s.id UNION ALL SELECT s.id, count(*) AS count2 FROM student AS s JOIN club_schedule AS clubsched ON s.id = clubsched.student_id JOIN(SELECT club_id FROM club_schedule WHERE student_id = 10) AS clubs WHERE clubsched.club_id = clubs.club_id AND s.id != 10 GROUP BY s.id ) AS d GROUP BY d.id;";
                 MySqlCommand command = new MySqlCommand(sql, con);
                 MySqlDataReader rdr = command.ExecuteReader();
