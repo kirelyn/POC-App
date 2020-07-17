b0b10d417904dfd81881db1194afa2109121b8e8
JohnnyZ67 <zielinskij@hotmail.com>
2020-07-17 13:10:18 -0500

    added rdr.read validation

--- POCAPP/InfectedStudentForm.cs
+++ POCAPP/InfectedStudentForm.cs
@@ -57,14 +57,19 @@ namespace POCAPP
                 string sql = "SELECT d.id, SUM(d.c) AS Count FROM( SELECT s.id, count(*) AS c FROM student AS s join class_schedule AS csched on s.id = csched.student_id JOIN(SELECT class_id FROM class_schedule WHERE student_id =" + id + ") AS classes WHERE csched.class_id = classes.class_id AND s.id !=" + id + " GROUP BY s.id UNION ALL SELECT s.id, count(*) AS count2 FROM student AS s JOIN club_schedule AS clubsched ON s.id = clubsched.student_id JOIN(SELECT club_id FROM club_schedule WHERE student_id =" + id+ ") AS clubs WHERE clubsched.club_id = clubs.club_id AND s.id != " + id + " GROUP BY s.id ) AS d GROUP BY d.id;";
                 MySqlCommand command = new MySqlCommand(sql, con);
                 MySqlDataReader rdr = command.ExecuteReader();
-               
-                
-                while (rdr.Read())
-                {
-                    MessageBox.Show(rdr[0] + " -- " + rdr[1]);
-                }
-                rdr.Close();
 
+                if (rdr.Read() == false)
+                {
+                    MessageBox.Show("No connection found.");
+                }
+                else
+                {
+                    while (rdr.Read())
+                    {
+                        MessageBox.Show(rdr[0] + " -- " + rdr[1]);
+                    }
+                    rdr.Close();
+                }
             }
             catch
             {
