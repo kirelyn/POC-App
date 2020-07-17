c16e448198cf6897aeb3c25da2dac74888964ff0
Kennedy Griffin <kennedyg@ksu.edu>
2020-07-17 11:25:04 -0500

    Can now enter student id and get priority returned.

--- POCAPP/ConnectionForm.cs
+++ POCAPP/ConnectionForm.cs
@@ -23,15 +23,13 @@ namespace POCAPP
             InitializeComponent();
         }
 
-        private void ConnectionForm_Load(object sender, EventArgs e)
-        {
-
-        }
+        
 
         //This is how the windows forms will be connected to the database. The connection will have to be personalized.
         string myConnectionString = "server=localhost;database=pocapp;uid=Kennedy;pwd=Django100.;";
         public void button1_Click(object sender, EventArgs e)
         {
+            
             //int id = 10;
             string connectionString = "server=localhost;database=pocapp;uid=Kennedy;pwd=Django100.;";
             MySqlConnection con = new MySqlConnection(connectionString); 
@@ -39,20 +37,17 @@ namespace POCAPP
             try
             {
                 con.Open();
-                MessageBox.Show("Connetion Successful!");
-                string sql = "SELECT d.id, SUM(d.c) AS Count FROM( SELECT s.id, count(*) AS c FROM student AS s join class_schedule AS csched on s.id = csched.student_id JOIN(SELECT class_id FROM class_schedule WHERE student_id = 10) AS classes WHERE csched.class_id = classes.class_id AND s.id != 10 GROUP BY s.id UNION ALL SELECT s.id, count(*) AS count2 FROM student AS s JOIN club_schedule AS clubsched ON s.id = clubsched.student_id JOIN(SELECT club_id FROM club_schedule WHERE student_id = 10) AS clubs WHERE clubsched.club_id = clubs.club_id AND s.id != 10 GROUP BY s.id ) AS d GROUP BY d.id;";
-                MySqlCommand command = new MySqlCommand(sql, con);
-                MySqlDataReader rdr = command.ExecuteReader();
-
-                while (rdr.Read())
-                {
-                    MessageBox.Show(rdr[0] + " -- " + rdr[1]);
-                }
-                rdr.Close();
+                MessageBox.Show("Connection Successful!");
+                this.Hide();
+                Form Form2 = new XEnterId();
+                Form2.ShowDialog();
+                this.Close();
+                this.Close();
             }
             catch
             {
                 MessageBox.Show("Connection Failed");
+                Close();
             }
 
             con.Close();
---
+++ POCAPP/InfectedStudentForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace POCAPP
+{
+    public partial class XEnterId : Form
+    {
+        public XEnterId()
+        {
+            InitializeComponent();
+            xStudentIdText.Select();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xFirstNameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        string myConnectionString = "server=localhost;database=pocapp;uid=Kennedy;pwd=Django100.;";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int id = Int32.Parse(xStudentIdText.Text);
+            string connectionString = "server=localhost;database=pocapp;uid=Kennedy;pwd=Django100.;";
+            MySqlConnection con = new MySqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                string sql = "SELECT d.id, SUM(d.c) AS Count FROM( SELECT s.id, count(*) AS c FROM student AS s join class_schedule AS csched on s.id = csched.student_id JOIN(SELECT class_id FROM class_schedule WHERE student_id =" + id + ") AS classes WHERE csched.class_id = classes.class_id AND s.id !=" + id + " GROUP BY s.id UNION ALL SELECT s.id, count(*) AS count2 FROM student AS s JOIN club_schedule AS clubsched ON s.id = clubsched.student_id JOIN(SELECT club_id FROM club_schedule WHERE student_id =" + id+ ") AS clubs WHERE clubsched.club_id = clubs.club_id AND s.id != " + id + " GROUP BY s.id ) AS d GROUP BY d.id;";
+                MySqlCommand command = new MySqlCommand(sql, con);
+                MySqlDataReader rdr = command.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    MessageBox.Show(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Connection Failed");
+            }
+
+            con.Close();
+        }
+    }
+}
