e2c2298e2baae30cb5ec3b87cd2e2dba36147c01
JohnnyZ67 <zielinskij@hotmail.com>
2020-07-17 13:06:11 -0500

    connection validation added

--- POCAPP/InfectedStudentForm.cs
+++ POCAPP/InfectedStudentForm.cs
@@ -49,7 +49,11 @@ namespace POCAPP
             // carrying over connection from the first form to avoid having two different opens. -- Johnny
             try
             {
-                //con.Open();
+                if(con.State != ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                //
                 string sql = "SELECT d.id, SUM(d.c) AS Count FROM( SELECT s.id, count(*) AS c FROM student AS s join class_schedule AS csched on s.id = csched.student_id JOIN(SELECT class_id FROM class_schedule WHERE student_id =" + id + ") AS classes WHERE csched.class_id = classes.class_id AND s.id !=" + id + " GROUP BY s.id UNION ALL SELECT s.id, count(*) AS count2 FROM student AS s JOIN club_schedule AS clubsched ON s.id = clubsched.student_id JOIN(SELECT club_id FROM club_schedule WHERE student_id =" + id+ ") AS clubs WHERE clubsched.club_id = clubs.club_id AND s.id != " + id + " GROUP BY s.id ) AS d GROUP BY d.id;";
                 MySqlCommand command = new MySqlCommand(sql, con);
                 MySqlDataReader rdr = command.ExecuteReader();
